107e492812102fff925df3e0719b3accabc2b52d
yummyweb <antriksh.timepass@gmail.com>
2025-06-06 13:53:46 +0800

    hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.0",
+  paths: {
+    sources: "./contracts",
+    tests: "./tests",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
+};
