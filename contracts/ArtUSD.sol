2fc657d70190fd51b3677462eab11ad934a72788
yummyweb <antriksh.timepass@gmail.com>
2025-06-10 18:48:27 +0800

    pausing and unpausing works with success tests

--- contracts/ArtUSD.sol
+++ contracts/ArtUSD.sol
@@ -4,17 +4,16 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol"; /* an example of price oracle */
+import "@openzeppelin/contracts/utils/Pausable.sol";
 
-contract ArtUSD is ERC20, Ownable {
+contract ArtUSD is ERC20, Ownable, Pausable {
     AggregatorV3Interface public artPriceFeed;
     address public fundPool;
-    bool public paused;
 
     event Redeemed(address indexed user, uint256 amount, string assetType);
 
     constructor(address initialOwner, address _artPriceFeed) ERC20("ArtUSD", "AUSD")  Ownable(initialOwner) {
         artPriceFeed = AggregatorV3Interface(_artPriceFeed);
-        paused = false;
     }
 
     // Set fund pool address
@@ -22,11 +21,7 @@ contract ArtUSD is ERC20, Ownable {
         fundPool = _fundPool;
     }
 
-    modifier whenNotPaused() {
-        require(!paused, "Contract is paused");
-        _;
-    }
-
+    // Required for minting more stablecoin tokens (either fund pool or the owner)
     modifier onlyOwnerOrFundPool() {
         require(msg.sender == owner() || msg.sender == fundPool, "Not authorized");
         _;
@@ -51,11 +46,12 @@ contract ArtUSD is ERC20, Ownable {
         return uint256(price);
     }
 
+    // Pausable functions, can only be called by owner of contract
     function pause() external onlyOwner {
-        paused = true;
+        _pause();
     }
 
     function unpause() external onlyOwner {
-        paused = false;
+        _unpause();
     }
 }
--- tests/ArtUSD.test.js
+++ tests/ArtUSD.test.js
@@ -1,23 +1,30 @@
 // test/ArtUSD.test.js
 // Load dependencies
-// const { expect } = require('chai');
-// const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers")
+const { expect } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers")
 
 // Start test block
-// describe('ArtUSD', function () {
-//     async function deployFixture() {
-//         const [owner] = await ethers.getSigners();
-//         const ArtUSD = await ethers.getContractFactory("artUSD");
-//         const artUSD = await ArtUSD.deploy(owner.address);
-//         await artUSD.waitForDeployment();
+describe('ArtUSD', function () {
+    async function deployFixture() {
+        const [owner] = await ethers.getSigners();
+        const ArtUSD = await ethers.getContractFactory("ArtUSD");
+        const artUSD = await ArtUSD.deploy(owner.address, owner.address);
+        await artUSD.waitForDeployment();
 
-//         return { artUSD, owner };
-//     }
+        return { artUSD, owner };
+    }
 
-//     describe('Deployment', function () {
-//         it('should deploy the contract', async function () {
-//             const { artUSD, owner } = await loadFixture(deployFixture);
-//             expect(await artUSD.owner()).to.equal(owner.address);
-//         });
-//     });
-// });
+    describe('Deployment', function () {
+        it('should deploy the contract', async function () {
+            const { artUSD, owner } = await loadFixture(deployFixture);
+            expect(await artUSD.owner()).to.equal(owner.address);
+        });
+    });
+
+    describe('Pause', function () {
+        it('should pause the contract', async function () {
+            const { artUSD, owner } = await loadFixture(deployFixture);
+            await expect(artUSD.pause()).to.emit(artUSD, "Paused");
+        });
+    });
+});
